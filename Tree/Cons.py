b8c841bf93e9e7fef87fbe460a8642a5e30c5648
Reuel D'silva <reuelrds@gmail.com>
2021-10-05 21:18:23 -0500

    Updated parseList method

--- Tree/Cons.py
+++ Tree/Cons.py
@@ -1,12 +1,12 @@
 # Cons -- Parse tree node class for representing a Cons node
 
-from Tree import Node
-from Tree import Ident
-
 import sys
 
+import Special
+import Tree
 
-class Cons(Node):
+
+class Cons(Tree.Node):
     def __init__(self, a, d):
         self.car = a
         self.cdr = d
@@ -21,20 +21,48 @@ class Cons(Node):
     # object from the Special hierarchy and to leave the rest of
     # parsing up to the interpreter.
     def parseList(self):
-        # TODO: implement this function and any helper functions
-        # you might need
-        self.form = None
+
+        if isinstance(self.car, Tree.Ident):
+
+            if self.car.getName() == "begin":
+                self.form = Special.Begin()
+
+            elif self.car.getName() == "cond":
+                self.form = Special.Cond()
+
+            elif self.car.getName() == "define":
+                self.form = Special.Define()
+
+            elif self.car.getName() == "if":
+                self.form = Special.If()
+
+            elif self.car.getName() == "lambda":
+                self.form = Special.Lambda()
+
+            elif self.car.getName() == "let":
+                self.form = Special.Let()
+
+            elif self.car.getName() == "quote":
+                self.form = Special.Quote()
+
+            elif self.car.getName() == "set":
+                self.form = Special.Set()
+
+            else:
+                self.form = Special.Regular()
+        else:
+            self.form = Special.Regular()
 
     def print(self, n, p=False):
-        # self.form.print(self, n, p)
-        sys.stdout.write("(")
-        self.car.print(n=-1)
-        sys.stdout.write(". ")
-        self.cdr.print(n=-1)
+        self.form.print(self, n, p)
+        # sys.stdout.write("(")
+        # self.car.print(n=-1)
+        # sys.stdout.write(". ")
+        # self.cdr.print(n=-1)
 
-        sys.stdout.write(")")
+        # sys.stdout.write(")")
 
 
 if __name__ == "__main__":
-    c = Cons(Ident("Hello"), Ident("World"))
+    c = Cons(Tree.Ident("Hello"), Tree.Ident("World"))
     c.print(0)
