f159e3c914705487ae9b9e973028d7b8ec8222c3
Reuel D'silva <reuelrds@gmail.com>
2021-10-07 02:23:46 -0500

    Implemented pretty-printing for set!

--- Special/Set.py
+++ Special/Set.py
@@ -1,13 +1,22 @@
 # Set -- Parse tree node strategy for printing the special form set!
 
+import sys
+
 from Special import Special
+
 
 class Set(Special):
     # TODO: Add fields and modify the constructor as needed.
     def __init__(self):
         pass
-    
+
     def print(self, t, n, p):
         # TODO: Implement this function.
-        pass
 
+        if n > 0:
+            sys.stdout.write("\n")
+
+        sys.stdout.write(' ' * n)
+        sys.stdout.write("(set! ")
+
+        t.getCdr().print(n=-(abs(n)+4), p=True)
