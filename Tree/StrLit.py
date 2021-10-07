7d6ac4b383bbf895955e27a0a3fb75360a50bfc5
Reuel D'silva <reuelrds@gmail.com>
2021-10-07 16:21:09 -0500

    Fixed printing StrLit where an extra space was printed after the string

--- Tree/StrLit.py
+++ Tree/StrLit.py
@@ -10,10 +10,12 @@ class StrLit(Node):
 
     def print(self, n, p=False):
 
+        if n > 0:
+            sys.stdout.write("\n")
+
         sys.stdout.write(' ' * n)
 
-        # sys.stdout.write("\"" + self.strVal + "\"\n")
-        sys.stdout.write("\"" + self.strVal + "\" ")
+        sys.stdout.write("\"" + self.strVal + "\"")
 
     def isString(self):
         return True
