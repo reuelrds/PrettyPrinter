1ae7219c7a6d5bd2a9c71c2e4819430d7f1b3281
Reuel D'silva <reuelrds@gmail.com>
2021-10-07 21:22:10 -0500

    Pretty Printing Lambda works

--- Special/Lambda.py
+++ Special/Lambda.py
@@ -1,6 +1,8 @@
 # Lambda -- Parse tree node strategy for printing the special form lambda
 
+import sys
 from Special import Special
+
 
 class Lambda(Special):
     # TODO: Add fields and modify the constructor as needed.
@@ -8,5 +10,20 @@ class Lambda(Special):
         pass
 
     def print(self, t, n, p):
-        # TODO: Implement this function.
-        pass
+        if n > 0:
+            sys.stdout.write("\n")
+
+        sys.stdout.write(' ' * n)
+        sys.stdout.write("(lambda ")
+
+        # THe variable names are from the function syntax
+        # at https://www.cs.cmu.edu/Groups/AI/html/r4rs/r4rs_6.html
+
+        formals = t.getCdr().getCar()
+        body = t.getCdr().getCdr().getCar()
+        end_paren = t.getCdr().getCdr().getCdr()
+
+        formals.print(n=-(abs(n)+4), p=False)
+        body.print(n=(abs(n)+4), p=False)
+
+        end_paren.print(n=(abs(n)+4), p=True)
